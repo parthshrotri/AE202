6f852dbe0384c33714cd43d8506239d0c8d3ebdb
Parth Shrotri <shrotri.parth@gmail.com>
2021-11-11 00:25:46 -0600

    set up for HW4

---
+++ AE202HW4.py
@@ -0,0 +1,24 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from  sympy import *
+
+###################CONSTANTS###################
+
+muEarth = 3.986*1000*10**5              #m^3/s^2
+muSun = 1.327*1000*10**11               #m^3/s^2
+radiusEarth = 6371 * 1000               #meters
+perihelionEarth = 147.09 *1000 * 10**6  #meters
+aphelionEarth = 152.10 * 1000 * 10**6   #meters
+
+minAlt = 20*1000                        #meters
+maxAlt = 40000*1000                     #meters
+
+###################PROBLEM 1###################
+
+###################PROBLEM 2###################
+
+###################PROBLEM 3###################
+
+###################PROBLEM 4###################
+
+###################PROBLEM 5###################
